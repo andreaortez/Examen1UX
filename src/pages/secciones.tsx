e06b04e69acd3d1ef7283647493e86205b768df7
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2024-11-08 22:54:47 -0600

    Ya esta el carusel de las cards

--- src/pages/secciones.tsx
+++ src/pages/secciones.tsx
@@ -1,56 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { Card, CardMedia,Typography, Grid } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { Card, CardMedia, Typography, useMediaQuery, useTheme, Grid } from '@mui/material';
+import { ShowChart } from '@mui/icons-material';
 
+//Interfaces
 interface Propiedades {
     titulo: string;
+    cards: { 
+        imagen: string;
+        titulo: string;
+    }[];
+}
+interface cardProp {
+    titulo: string;
+    imagen: string;
 }
 
-function Secciones({titulo}:Propiedades) {
-    const items = [
-        {
-            image: "https://occ-0-8407-999.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRVCeeNG3zlLs43tI7tLeQCJTa9If46uKut6AEZEmDf1U_ig3LltqQw_3tuX__gPA60nlAKA34EnEwGPee_I1aP4zkGqtTGJn7sGwq_Ql4fUCT-VF21mlpWtH3USLPxk2s9ysg.jpg?r=bdf",
-            title: "Anne with an E"
-        },
-        {
-            image: "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXHBPcqLrRXU-c0-t3vw2PYAaHfaOtbLYlLluR_XiJ9t5awR1TvlN6BKVJj89gSRFW1rxi8-SC0OjhSMfh446rlbdkR8w_mKv6qJkKi36y1sF4xg40RBMdVma-PylYS4FZibt6VFMHyORksAy1k4TFp6t6-rGbhgBuIHVwNfhTdfGD4vHp97yekJwJaTLs4rwxHzc1XSkKtXWF1iH9Z7mJGFSCcCM1NIeEJCzQNQJsaQeUcUvwUgoqEHw2EXrA44EpzcJZsQR6gEaCkzNZBszCIE-wC8G04ePr49UFwLZcM5WTKRBcpAf62O-Li6S47dGVxMBNVVgcLjxR3LFT5GBt7k.webp?r=8e5",
-            title: "Gossip Girl"
-        },
-        {
-            image: "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABfcHrq_UTs2hUAYWArWmZOZzkFDi-m9eT-z7Y-in5OSegW4lUAAiKqcV6PYkaKP_gfGyeKTcJP_PpuGu9Fx0LXOsGTKzEnEVV2gX.jpg?r=38f",
-            title: "Twilight"
-        },
-        {
-            image: "https://occ-0-8407-999.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRVCeeNG3zlLs43tI7tLeQCJTa9If46uKut6AEZEmDf1U_ig3LltqQw_3tuX__gPA60nlAKA34EnEwGPee_I1aP4zkGqtTGJn7sGwq_Ql4fUCT-VF21mlpWtH3USLPxk2s9ysg.jpg?r=bdf",
-            title: "Anne with an 0"
-        },
-        {
-            image: "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXHBPcqLrRXU-c0-t3vw2PYAaHfaOtbLYlLluR_XiJ9t5awR1TvlN6BKVJj89gSRFW1rxi8-SC0OjhSMfh446rlbdkR8w_mKv6qJkKi36y1sF4xg40RBMdVma-PylYS4FZibt6VFMHyORksAy1k4TFp6t6-rGbhgBuIHVwNfhTdfGD4vHp97yekJwJaTLs4rwxHzc1XSkKtXWF1iH9Z7mJGFSCcCM1NIeEJCzQNQJsaQeUcUvwUgoqEHw2EXrA44EpzcJZsQR6gEaCkzNZBszCIE-wC8G04ePr49UFwLZcM5WTKRBcpAf62O-Li6S47dGVxMBNVVgcLjxR3LFT5GBt7k.webp?r=8e5",
-            title: "Gossip Girl"
-        },
-        // elementos
-    ];
+
+//Funciones
+function Secciones({ titulo, cards }: Propiedades) {
+    //Variables fijas
+    const [showCards, setShowCards] = useState(6);
+    const smallScreen = useMediaQuery(useTheme().breakpoints.down('sm'));
+    const mediumScreen = useMediaQuery(useTheme().breakpoints.down('md'));
+    const largeScreen = useMediaQuery(useTheme().breakpoints.down('lg'));
+    const xlargeScreen = useMediaQuery(useTheme().breakpoints.down('xl'));
+    const row = () => {
+
+
+        let n_stack = Math.ceil(cards.length / showCards);
+        return (
+            Array.from({ length: n_stack }).map((item, i) =>
+                <Stack key={i} direction={"row"} spacing={1} sx={{ justifyContent: "center" }}>
+                    {
+                        cards.slice(i * showCards, i * showCards + showCards).map((card, index) => (
+                            <Item key={index} imagen={card.imagen} titulo={card.titulo} />
+                        ))
+                    }
+                </Stack>
+            )
+        )
+    }
+
+    useEffect(() => {
+        if (smallScreen) {
+            setShowCards(2);
+        } else if (mediumScreen) {
+            setShowCards(3);
+        }
+        else if (largeScreen) {
+            setShowCards(4);
+        } else if (xlargeScreen) {
+            setShowCards(6);
+        }
+    }, [smallScreen, mediumScreen, largeScreen, xlargeScreen]);
+
     return (
         <div>
-            <Typography variant='h6'>{titulo}</Typography>
-            <Carousel indicators={false} autoPlay={false} sx={{ width: '100vw' }}>
-                {
-                    items.map((item, i) => <Item key={i} item={item} />)
-                }
+            <Typography
+                variant='h6'
+                sx={{
+                    mt: { xs: 0.5, sm: 1, md: 1.5 }, 
+                    mb: { xs: 0.5, sm: 1, md: 1.5 }, 
+                    ml: { xs: 1, sm: 2, md: 3 }, 
+                }}
+            >
+                {titulo}
+            </Typography>
+    
+            <Carousel
+                indicators={false}
+                autoPlay={false}
+                sx={{
+                    width: '95vw',
+                    mt: { xs: 0.5, sm: 1, md: 1.5 }, 
+                    mb: { xs: 0.5, sm: 1, md: 1.5 }, 
+                    ml: { xs: 1, sm: 2, md: 3 }, 
+                }}
+            >
+                {row()}
             </Carousel>
         </div>
     )
+    
 }
 
-function Item(props: any) {
+function Item({ imagen, titulo }: cardProp) {
     return (
-        <Card sx={{ maxWidth: '20vw' }}>
+        <Card sx={{ flexBasis: { xs: "45%", sm: "30%", md: "20%" }, maxWidth: "100%" }}>
 
             <CardMedia
                 component="img"
-                height="140"
-                image={props.item.image}
-                alt={props.item.title}
+                image={imagen}
+                alt={titulo}
             />
         </Card>
     )
